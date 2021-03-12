26d7b011ae75dc5e06eb15c5e920acd46f176c25
Gehrig Wilcox <lougehrig10@gmail.com>
2021-03-11 20:13:50 -0600

    Added main functions for ProcessManager

---
+++ src/ProcessManager/ProcessManager.c
@@ -0,0 +1,44 @@
+/*
+
+  ProcessManager.c
+
+  Gehrig Wilcox <lougehrig10@gmail.com>
+
+  3/9/21
+
+*/
+
+#include "DataStructures/LinkedList/DoublyLinkedList.h"
+
+
+doublyLinkedList_t* processList;
+
+
+/*
+
+  Allocates system resources to new process, loads program into memory, and adds
+  process to process list.
+
+*/
+void createNewProcess(){
+
+}
+
+/*
+
+  Save current system state, associate it with the current process, then choose
+  next process from list and load its saved system state.
+
+*/
+void switchProcess(){
+
+}
+
+/*
+
+  Removes process from process list, and frees up allocated system resources.
+
+*/
+void destroyProcess(){
+
+}
